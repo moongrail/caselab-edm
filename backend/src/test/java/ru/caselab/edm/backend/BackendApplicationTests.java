8cb1b5389dff54884dd5310dbccd9857f5372708
Moongrail <omgone666@yandex.ru>
2024-10-03 23:03:56 +0300

    feat: ПОГНАЛИ РЕБЯТ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- backend/src/test/java/ru/caselab/edm/backend/BackendApplicationTests.java
+++ backend/src/test/java/ru/caselab/edm/backend/BackendApplicationTests.java
@@ -1,13 +1,17 @@
 package ru.caselab.edm.backend;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class BackendApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertNotNull(context);
 	}
 
 }
