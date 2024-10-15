65f2ebf3483b670e612fb042dae6cb288feb260d
Klimanova Evgenia <mgenya22@gmail.com>
2024-10-16 02:19:52 +0300

    delete unused imports

--- backend/src/main/java/ru/caselab/edm/backend/entity/Document.java
+++ backend/src/main/java/ru/caselab/edm/backend/entity/Document.java
@@ -1,8 +1,13 @@
 package ru.caselab.edm.backend.entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
