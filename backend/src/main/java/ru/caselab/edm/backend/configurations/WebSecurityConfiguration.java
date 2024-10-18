8f4c6762e6d8dc9b52b3c70480828e61d4ab4ca5
ndtm1 <degtyarevn5@gmail.com>
2024-10-18 20:09:38 +0400

    fix: dao

--- backend/src/main/java/ru/caselab/edm/backend/configurations/WebSecurityConfiguration.java
+++ backend/src/main/java/ru/caselab/edm/backend/configurations/WebSecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,13 +15,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import ru.caselab.edm.backend.filter.JwtFilter;
 import ru.caselab.edm.backend.security.details.UserDetailsServiceImpl;
-import ru.caselab.edm.backend.security.provider.CustomAuthenticationProvider;
 
 import java.util.Arrays;
 
@@ -63,7 +62,7 @@ public class WebSecurityConfiguration {
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().authenticationProvider(customAuthenticationProvider())
+                .and().authenticationProvider(new DaoAuthenticationProvider())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class).build();
 
     }
@@ -76,11 +75,6 @@ public class WebSecurityConfiguration {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
-    }
-
-    @Bean
-    public AuthenticationProvider customAuthenticationProvider() {
-        return new CustomAuthenticationProvider();
     }
 
     @Bean
