e7de63748585550bc1a47f2b8e2a5d891f0bf6f5
ndtm1 <degtyarevn5@gmail.com>
2024-10-18 07:41:24 +0400

    feat: custom auth provider

--- backend/src/main/java/ru/caselab/edm/backend/configurations/WebSecurityConfiguration.java
+++ backend/src/main/java/ru/caselab/edm/backend/configurations/WebSecurityConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,8 +15,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import ru.caselab.edm.backend.filter.JwtFilter;
 import ru.caselab.edm.backend.security.details.UserDetailsServiceImpl;
+import ru.caselab.edm.backend.security.provider.CustomAuthenticationProvider;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -31,12 +36,10 @@ public class WebSecurityConfiguration {
             "/swagger-resources/**",
             "/swagger-resources",
             "/api/v1/check/**",
-            "/api/v1/users/**",
             "/api/v1/jwt/**",
             "/swagger/**",
-            "/check/**",
-            "/users/**",
-            "/jwt/**"
+            "/jwt/**",
+            "/api/v1/error"
     };
     final JwtFilter jwtFilter;
 
@@ -52,17 +55,15 @@ public class WebSecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
+        return http.headers().httpStrictTransportSecurity().disable().and()
                 .cors().and()
                 .csrf().disable()
                 .authorizeHttpRequests().requestMatchers(WHITELIST)
-                .permitAll()
-                .and()
-                .authorizeHttpRequests().requestMatchers(new AntPathRequestMatcher("/api/v1/**")).authenticated()
+                .permitAll().requestMatchers("/error").permitAll().anyRequest().authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().authenticationProvider(authenticationProvider())
+                .and().authenticationProvider(customAuthenticationProvider())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class).build();
 
     }
@@ -72,16 +73,33 @@ public class WebSecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService());
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        return authenticationProvider;
-    }
+//    @Bean
+//    public AuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+//        authenticationProvider.setUserDetailsService(userDetailsService());
+//        authenticationProvider.setPasswordEncoder(passwordEncoder());
+//        return authenticationProvider;
+//    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider customAuthenticationProvider() {
+        return new CustomAuthenticationProvider();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
