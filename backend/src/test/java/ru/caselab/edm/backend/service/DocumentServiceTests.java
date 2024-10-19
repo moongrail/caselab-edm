b30d989f9da3c80b55930bfd3ce3dc5d51f3bed1
ndtm1 <degtyarevn5@gmail.com>
2024-10-19 14:29:52 +0400

    fix: added anyLong()

--- backend/src/test/java/ru/caselab/edm/backend/service/DocumentServiceTests.java
+++ backend/src/test/java/ru/caselab/edm/backend/service/DocumentServiceTests.java
@@ -31,6 +31,7 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
