fbf7ad050f2ea7150e9ca7f9e2696cd860da2079
ndtm1 <degtyarevn5@gmail.com>
2024-10-19 16:31:18 +0400

    style: swagger

--- backend/src/main/java/ru/caselab/edm/backend/controllers/DocumentController.java
+++ backend/src/main/java/ru/caselab/edm/backend/controllers/DocumentController.java
@@ -3,6 +3,7 @@ package ru.caselab.edm.backend.controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -27,7 +28,6 @@ import ru.caselab.edm.backend.dto.*;
 import ru.caselab.edm.backend.entity.UserInfoDetails;
 import ru.caselab.edm.backend.mapper.DocumentMapper;
 import ru.caselab.edm.backend.mapper.DocumentVersionMapper;
-import ru.caselab.edm.backend.repository.UserRepository;
 import ru.caselab.edm.backend.service.DocumentService;
 import ru.caselab.edm.backend.service.SignatureService;
 
@@ -44,11 +44,18 @@ public class DocumentController {
     private final DocumentMapper documentMapper;
     private final SignatureService signatureService;
     private final DocumentVersionMapper documentVersionMapper;
-    private final UserRepository userRepository;
 
+    @Operation(summary = "Sign document with given id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Document was successfully signed",
+                    content = @Content),
+            @ApiResponse(responseCode = "400", description = "Signature already exists")
+    })
     @PostMapping("/{id}/sign")
     @ResponseStatus(HttpStatus.OK)
-    public void signDocument(@Valid @RequestBody SignatureCreateDTO signatureCreateDTO, @PathVariable Long id) {
+    public void signDocument(@Valid @RequestBody SignatureCreateDTO signatureCreateDTO,
+                             @Parameter(description = "Document id", required = true, example = "1")
+                             @PathVariable Long id) {
         signatureService.sign(signatureCreateDTO, id);
     }
 
@@ -72,13 +79,21 @@ public class DocumentController {
     }
 
 
-
+    @Operation(summary = "Creation document and first version of this document")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Document and version successfully created",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = DocumentVersionDTO.class))),
+            @ApiResponse(responseCode = "404", description = "Document/User not found", content = @Content)
+    })
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
-    public DocumentVersionDTO createDocumentType(@Valid @RequestBody DocumentCreateDTO documentTypeCreateDTO) {
-        return documentVersionMapper.toDto(documentService.saveDocument(documentTypeCreateDTO));
+    public DocumentVersionDTO createDocument(@Valid @RequestBody DocumentCreateDTO documentCreateDTO) {
+        return documentVersionMapper.toDto(documentService.saveDocument(documentCreateDTO));
     }
 
+    @Operation(summary = "Returning all documents of the current user")
+    @ApiResponse(responseCode = "200", description = "Documents of the current user were successfully returned",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = DocumentPageDTO.class)))
     @GetMapping()
     @ResponseStatus(HttpStatus.OK)
     public DocumentPageDTO getAllDocuments(@RequestParam(name = "page", defaultValue = "0") @Min(value = 0) int page,
@@ -87,23 +102,44 @@ public class DocumentController {
         return documentMapper.toDtoPage(documentService.getAllDocumentForUser(page, size, user.getId()));
     }
 
+    @Operation(summary = "Returning document of the current user by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Document of the current user was successfully returned",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = DocumentDTO.class))),
+            @ApiResponse(responseCode = "403", description = "Access to the document is forbidden",
+                    content = @Content)
+    })
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public DocumentDTO getDocumentById(@PathVariable Long id,
+    public DocumentDTO getDocumentById(
+            @Parameter(description = "Document id", required = true, example = "1")
+            @PathVariable Long id,
                                        @AuthenticationPrincipal UserInfoDetails user) {
         return documentMapper.toDto(documentService.getDocumentForUser(id, user.getId()));
     }
 
+    @Operation(summary = "Updating document fields")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Document fields was successfully updated",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = DocumentVersionDTO.class))),
+            @ApiResponse(responseCode = "404", description = "User/Document/Document type not found", content = @Content)
+    })
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public DocumentVersionDTO updateDocumentType(@PathVariable Long id,
-                                          @RequestBody @Valid DocumentUpdateDTO updateDocumentType) {
-        return documentVersionMapper.toDto(documentService.updateDocument(id, updateDocumentType));
+    public DocumentVersionDTO updateDocument(
+            @Parameter(description = "Document id", required = true, example = "1")
+            @PathVariable Long id,
+                                          @RequestBody @Valid DocumentUpdateDTO updateDocument) {
+        return documentVersionMapper.toDto(documentService.updateDocument(id, updateDocument));
     }
 
+    @Operation(summary = "Deleting document")
+    @ApiResponse(responseCode = "204", description = "Document was successfully deleted", content = @Content)
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteDocument(@PathVariable Long id) {
+    public void deleteDocument(
+            @Parameter(description = "Document id", required = true, example = "1")
+            @PathVariable Long id) {
         documentService.deleteDocument(id);
     }
 }
