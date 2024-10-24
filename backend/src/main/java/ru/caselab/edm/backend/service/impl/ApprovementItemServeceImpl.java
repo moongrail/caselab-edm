01678dba17943c839c291621ccf49287c4845625
larentina <118663828+larentiina@users.noreply.github.com>
2024-10-24 14:32:05 +0300

    fix: add annotations service

--- backend/src/main/java/ru/caselab/edm/backend/service/impl/ApprovementItemServeceImpl.java
+++ backend/src/main/java/ru/caselab/edm/backend/service/impl/ApprovementItemServeceImpl.java
@@ -1,10 +1,12 @@
 package ru.caselab.edm.backend.service.impl;
 
+import org.springframework.stereotype.Service;
 import ru.caselab.edm.backend.entity.ApprovementProcessItem;
 import ru.caselab.edm.backend.service.ApprovementItemService;
 
 import java.util.UUID;
 
+@Service
 public class ApprovementItemServeceImpl implements ApprovementItemService {
     @Override
     public ApprovementProcessItem createItemForAprovementProcess(UUID UserId, Long documentVersionId, Long processId) {
--- backend/src/main/java/ru/caselab/edm/backend/service/impl/ApprovementServiceImpl.java
+++ backend/src/main/java/ru/caselab/edm/backend/service/impl/ApprovementServiceImpl.java
@@ -1,9 +1,11 @@
 package ru.caselab.edm.backend.service.impl;
 
+import org.springframework.stereotype.Service;
 import ru.caselab.edm.backend.dto.ApprovementProcessCreateDTO;
 import ru.caselab.edm.backend.dto.ApprovementProcessDTO;
 import ru.caselab.edm.backend.service.ApprovementService;
 
+@Service
 public class ApprovementServiceImpl implements ApprovementService {
 
     @Override
