1f21e63a898fb6ee24199a181a7ee1a0e2cf3f7e
ndtm1 <degtyarevn5@gmail.com>
2024-10-27 18:35:15 +0400

    feat: status field

--- backend/src/main/java/ru/caselab/edm/backend/dto/SignatureCreateDTO.java
+++ backend/src/main/java/ru/caselab/edm/backend/dto/SignatureCreateDTO.java
@@ -1,8 +1,10 @@
 package ru.caselab.edm.backend.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,9 +21,13 @@ import java.util.UUID;
 public class SignatureCreateDTO {
     @JsonProperty("userId")
     @NotNull
+    @Schema(description = "User ID", format = "uuid", example = "123e4567-e89b-12d3-a456-426614174000")
     private UUID userId;
 
-    @JsonProperty("createdAt")
-    private LocalDateTime creationDate;
+    @JsonProperty("status")
+    @NotNull
+    @Pattern(regexp = "AGREE|NOTAGREE", message = "Status must be either AGREE or NOTAGREE")
+    @Schema(description = "Status of sign (does user agree/not agree)", example = "AGREE")
+    private String status;
 
 }
