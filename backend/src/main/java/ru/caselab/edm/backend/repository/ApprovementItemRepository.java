d60b449a850d5a6250883dc6685ad1052d6fca04
ndtm1 <degtyarevn5@gmail.com>
2024-10-27 18:44:33 +0400

    feat: find by doc version id and user id

--- backend/src/main/java/ru/caselab/edm/backend/repository/ApprovementItemRepository.java
+++ backend/src/main/java/ru/caselab/edm/backend/repository/ApprovementItemRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.caselab.edm.backend.entity.ApprovementProcessItem;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ApprovementItemRepository extends CrudRepository<ApprovementProcessItem, Long> {
     boolean existsByDocumentVersionIdAndUserId(Long documentVersionId, UUID userId);
+
+    Optional<ApprovementProcessItem> findByDocumentVersionIdAndUserId(Long documentVersionId, UUID userId);
 
 }
