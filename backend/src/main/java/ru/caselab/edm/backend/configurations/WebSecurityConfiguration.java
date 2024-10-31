2801768bd877248d362d84dce0d32e012f88fe95
ndtm1 <degtyarevn5@gmail.com>
2024-10-31 16:25:03 +0400

    feat: access to user creating without role

--- backend/src/main/java/ru/caselab/edm/backend/configurations/WebSecurityConfiguration.java
+++ backend/src/main/java/ru/caselab/edm/backend/configurations/WebSecurityConfiguration.java
@@ -3,6 +3,7 @@ package ru.caselab.edm.backend.configurations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -56,6 +57,7 @@ public class WebSecurityConfiguration {
                 .authorizeHttpRequests(auth -> auth
                                 .requestMatchers(WHITELIST).permitAll()
                                 .requestMatchers("/error").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/users").permitAll()
                                 .requestMatchers("/auth").permitAll()
                                 .anyRequest().authenticated()
                 )
