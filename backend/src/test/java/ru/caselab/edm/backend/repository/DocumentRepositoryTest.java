1a5b93aa6db3d83c2f3186405a4ac51e7dcdce29
Klimanova Evgenia <mgenya22@gmail.com>
2024-11-02 14:05:19 +0300

    fix data issue in repository test

--- backend/src/test/java/ru/caselab/edm/backend/repository/DocumentRepositoryTest.java
+++ backend/src/test/java/ru/caselab/edm/backend/repository/DocumentRepositoryTest.java
@@ -42,16 +42,16 @@ class DocumentRepositoryTest {
                 INSERT INTO document_types (id, name, description, created_at, updated_at) VALUES
                     (1, 'test_type', 'test', '2024-01-15', '2024-01-15');
                 INSERT INTO documents (id, user_id, document_type_id, created_at) VALUES
-                    (1,'48bbbd31-45c0-43c5-b989-c1c14a8c3b8b',1, '2024-01-15'),
-                    (2,'c5df47fe-f4d2-45c2-8084-e646c85a7eba',1, '2024-01-15'),
-                    (3,'48bbbd31-45c0-43c5-b989-c1c14a8c3b8b',1, '2024-01-15'),
-                    (4,'c5df47fe-f4d2-45c2-8084-e646c85a7eba',1, '2024-01-15');
+                    (1,'48bbbd31-45c0-43c5-b989-c1c14a8c3b8b',1, '2024-01-15'::TIMESTAMP AT TIME ZONE 'UTC'),
+                    (2,'c5df47fe-f4d2-45c2-8084-e646c85a7eba',1, '2024-01-15'::TIMESTAMP AT TIME ZONE 'UTC'),
+                    (3,'48bbbd31-45c0-43c5-b989-c1c14a8c3b8b',1, '2024-01-15'::TIMESTAMP AT TIME ZONE 'UTC'),
+                    (4,'c5df47fe-f4d2-45c2-8084-e646c85a7eba',1, '2024-01-15'::TIMESTAMP AT TIME ZONE 'UTC');
                 INSERT INTO document_versions (id, documents_id, document_name, created_at, updated_at, content_url) VALUES
-                    (1, 1, 'document_name_test1', '2024-01-18', '2024-01-15', 'test_url'),
-                    (2, 2, 'document_name_test2', '2024-01-15', '2024-01-15', 'test_url1'),
-                    (3, 3, 'document_name_test3', '2024-01-15', '2024-01-15', 'test_url3'),
-                    (4, 4, 'document_name_test4', '2024-01-15', '2024-01-15', 'test_url4'),
-                    (5, 1, 'document_name_test1', '2024-01-16', '2024-01-15', 'test_url');
+                    (1, 1, 'document_name_test1', '2024-01-18'::TIMESTAMP AT TIME ZONE 'UTC', '2024-01-15', 'test_url'),
+                    (2, 2, 'document_name_test2', '2024-01-15'::TIMESTAMP AT TIME ZONE 'UTC', '2024-01-15', 'test_url1'),
+                    (3, 3, 'document_name_test3', '2024-01-15'::TIMESTAMP AT TIME ZONE 'UTC', '2024-01-15', 'test_url3'),
+                    (4, 4, 'document_name_test4', '2024-01-15'::TIMESTAMP AT TIME ZONE 'UTC', '2024-01-15', 'test_url4'),
+                    (5, 1, 'document_name_test1', '2024-01-16'::TIMESTAMP AT TIME ZONE 'UTC', '2024-01-15', 'test_url');
                 INSERT INTO approvement_process(id, document_version_id, status, deadline, agreement_procent) VALUES
                     (1, 1, 'NOTACCEPTED', '2024-01-15', 1.00),
                     (2, 3, 'NOTACCEPTED', '2024-01-15', 1.00),
@@ -87,15 +87,15 @@ class DocumentRepositoryTest {
 
         List<DocumentOutputAllDocumentsDTO> expected = List.of(
                 new DocumentOutputAllDocumentsDTO("Ivanov",
-                        Instant.parse("2024-01-14T21:00:00Z").atZone(ZoneId.of("UTC")).toInstant(),
+                        LocalDateTime.parse("2024-01-15T00:00:00").atZone(ZoneId.systemDefault()).toInstant(),
                         "document_name_test2",
                         ApprovementProcessStatus.ACCEPTED),
                 new DocumentOutputAllDocumentsDTO("Ivanov",
-                        Instant.parse("2024-01-14T21:00:00Z").atZone(ZoneId.of("UTC")).toInstant(),
+                        LocalDateTime.parse("2024-01-15T00:00:00").atZone(ZoneId.systemDefault()).toInstant(),
                         "document_name_test1",
                         ApprovementProcessStatus.NOTACCEPTED),
                 new DocumentOutputAllDocumentsDTO("Ivanov",
-                        Instant.parse("2024-01-14T21:00:00Z").atZone(ZoneId.of("UTC")).toInstant(),
+                        LocalDateTime.parse("2024-01-15T00:00:00").atZone(ZoneId.systemDefault()).toInstant(),
                         "document_name_test3",
                         ApprovementProcessStatus.NOTACCEPTED)
         );
