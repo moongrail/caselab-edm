542d14068b5c65921fc14a659f5212bf8b231462
moongrail <marselfacegrail@gmail.com>
2024-11-11 21:14:56 +0300

    fix: fix tests

--- backend/src/test/java/ru/caselab/edm/backend/BackendApplicationTests.java
+++ backend/src/test/java/ru/caselab/edm/backend/BackendApplicationTests.java
@@ -1,15 +1,21 @@
 package ru.caselab.edm.backend;
 
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MinIOContainer;
+import ru.caselab.edm.backend.repository.elastic.AttributeSearchRepository;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@RequiredArgsConstructor
 class BackendApplicationTests {
-
+    @MockBean
+    AttributeSearchRepository attributeSearchRepository;
     private static final String MINIO_IMAGE = "minio/minio:RELEASE.2024-02-17T01-15-57Z.fips";
     private static final String MINIO_ENDPOINT = "minio.config.endpoint";
     private static final String MINIO_USERNAME = "minio.config.username";
@@ -28,30 +34,7 @@ class BackendApplicationTests {
         registry.add(MINIO_PASSWORD, minioContainer::getPassword);
     }
 
-
-/**
- *      Объяснения почему так сделал
- *      Контейнер без впн не качается.
- *      Плюс надо его еще настраивать.
- *      Везде где задевает ставьте @MockBean.
- TODO:  На случай если кому то захочется донастроить.
- */
-//    private static final String ELASTICSEARCH_IMAGE = "docker.elastic.co/elasticsearch/elasticsearch:8.7.1";
-
-//    @DynamicPropertySource
-//    static void elasticsearchProperties(DynamicPropertyRegistry registry) {
-//        registry.add("spring.elasticsearch.rest.uris", elasticsearchContainer::getHttpHostAddress);
-//        registry.add("spring.elasticsearch.username", () -> "user");
-//        registry.add("spring.elasticsearch.password", () -> "pass");
-//        registry.add("spring.data.elasticsearch.repositories.enabled", () -> "false");
-//    }
-
-//    static final ElasticsearchContainer elasticsearchContainer = new ElasticsearchContainer(ELASTICSEARCH_IMAGE)
-//            .withAccessToHost(true)
-//            .withSharedMemorySize(512L)
-//            .withPassword("pass");
-//
-//    static {
-//        elasticsearchContainer.start();
-//    }
+    @Test
+    public void contextLoads() {
+    }
 }
