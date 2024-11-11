08d4e620eb7d94c3ce917e6d26b489422895a3a7
Maevgal <143836952+Maevgal@users.noreply.github.com>
2024-11-11 20:32:36 +0300

    Fix/update_document_type (#102)
    * Update get document logic
    
    * fix:signature
    
    * Update get document where user signatories and update test for new logic
    
    * fix update document type
    
    ---------
    
    Co-authored-by: Saynurdinov Shukhrat <saynurdinov1998@gmail.com>

--- backend/src/main/java/ru/caselab/edm/backend/dto/documenttype/DocumentTypeUpdateDTO.java
+++ backend/src/main/java/ru/caselab/edm/backend/dto/documenttype/DocumentTypeUpdateDTO.java
@@ -2,6 +2,7 @@ package ru.caselab.edm.backend.dto.documenttype;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.Set;
@@ -10,10 +11,9 @@ import java.util.Set;
 @Data
 public class DocumentTypeUpdateDTO {
     @Schema(description = "Document type name", example = "contract")
-    @NotBlank
     private String name;
 
-    @Schema(description = "Description", example = "text document with external contractors", nullable = true)
+    @Schema(description = "Description document type", example = "text document with external contractors", nullable = true)
     private String description;
 
     @Schema(description = "Attributes", example = "[1, 2]", nullable = true)
