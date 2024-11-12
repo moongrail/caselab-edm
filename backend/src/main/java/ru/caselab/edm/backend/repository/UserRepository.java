79a7f7ae86af8854e866f5d362e32fc4fa2fb2a6
Klimanova Evgenia <mgenya22@gmail.com>
2024-11-12 13:28:32 +0300

    Creating a feature for finding employees to replace

--- backend/src/main/java/ru/caselab/edm/backend/repository/UserRepository.java
+++ backend/src/main/java/ru/caselab/edm/backend/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package ru.caselab.edm.backend.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.caselab.edm.backend.entity.User;
 
@@ -14,4 +17,16 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByLogin(String login);
 
     Optional<User> findUserByLogin(String username);
+
+    @Query(value = """
+            SELECT * FROM  users u
+            WHERE u.department_id IN (select department_id from users u2 where id = :userId)
+            AND id !=:userId
+            """, countQuery = """
+            SELECT * FROM  users u
+            WHERE u.department_id IN (select department_id from users u2 where id = :userId)
+            AND id !=:userId
+            """,
+            nativeQuery = true)
+    Page<User> getAllUserForReplacement(UUID userId, Pageable pageable);
 }
---
+++ backend/src/main/java/ru/caselab/edm/backend/service/impl/ReplacementManagementServiceImpl.java
@@ -0,0 +1,26 @@
+package ru.caselab.edm.backend.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import ru.caselab.edm.backend.entity.User;
+import ru.caselab.edm.backend.repository.UserRepository;
+import ru.caselab.edm.backend.service.ReplacementManagementService;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ReplacementManagementServiceImpl implements ReplacementManagementService {
+    private final UserRepository userRepository;
+
+    @Override
+    public Page<User> getUserForReplacement(int page, int size, UUID userId) {
+        PageRequest pageable = PageRequest.of(page, size);
+        log.info("Get a page of users available for replacement - page: {}, size: {}", page, size);
+        return userRepository.getAllUserForReplacement(userId, pageable);
+    }
+}
