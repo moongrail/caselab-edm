050f6dfcdf10b9f5701ab57e669d6e5ea76581ad
ndtm1 <degtyarevn5@gmail.com>
2024-11-13 21:35:13 +0400

    feat: update fields to new ERM

--- backend/src/main/java/ru/caselab/edm/backend/entity/User.java
+++ backend/src/main/java/ru/caselab/edm/backend/entity/User.java
@@ -1,11 +1,7 @@
 package ru.caselab.edm.backend.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 import java.util.Set;
@@ -16,6 +12,7 @@ import java.util.UUID;
 @Builder
 @Getter
 @Setter
+@EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
@@ -24,10 +21,6 @@ public class User {
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id")
     private UUID id;
-
-    @ManyToOne
-    @JoinColumn(name = "department_id", nullable = false)
-    private Department departmentId;
 
     @Column(name = "login", nullable = false, unique = true)
     private String login;
@@ -57,10 +50,10 @@ public class User {
     private Set<Document> documents;
 
     @OneToMany(mappedBy = "managerUserId")
-    private List<ReplacementManager> replacementManagers;
+    private Set<ReplacementManager> replacementManagers;
 
     @OneToMany(mappedBy = "tempManagerUserId")
-    private List<ReplacementManager> tempReplacementManagers;
+    private Set<ReplacementManager> tempReplacementManagers;
 
 /*    @OneToMany(mappedBy = "user")
     private Set<Signature> signatures;*/
@@ -72,6 +65,12 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
     private Set<Role> roles;
+
+    @ManyToMany(mappedBy = "managers")
+    private Set<Department> leadsDepartments;
+
+    @ManyToMany(mappedBy = "members")
+    private Set<Department> departments;
 
     @OneToMany(mappedBy = "user")
     private List<ApprovementProcessItem> approvementProcessItems;
