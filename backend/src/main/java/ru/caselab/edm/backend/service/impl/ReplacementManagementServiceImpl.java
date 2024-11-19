57010ae626f521b84d05be7fb6ef60b571b5d5ff
Maevgal <143836952+Maevgal@users.noreply.github.com>
2024-11-19 13:49:10 +0300

    Feature/search for employees to replace (#114)
    * Creating a feature for finding employees to replace
    
    * Creating a feature for finding employees to replace
    
    * Updating a feature for finding employees to replace in accordance with the new scheme
    
    * Updating a feature for finding employees to replace in accordance with the new scheme
    
    * Updating a feature for finding employees to replace in accordance with the new scheme
    
    * Updating a feature for finding employees to replace in accordance with the new logic: an employee can only work in one department and a department can have only one manager
    
    * Optimize import
    
    ---------
    
    Co-authored-by: Shukhrat <114294133+1shukhrat@users.noreply.github.com>

--- backend/src/main/java/ru/caselab/edm/backend/service/impl/ReplacementManagementServiceImpl.java
+++ backend/src/main/java/ru/caselab/edm/backend/service/impl/ReplacementManagementServiceImpl.java
@@ -3,12 +3,16 @@ package ru.caselab.edm.backend.service.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import ru.caselab.edm.backend.entity.User;
+import ru.caselab.edm.backend.repository.DepartmentRepository;
 import ru.caselab.edm.backend.repository.UserRepository;
 import ru.caselab.edm.backend.service.ReplacementManagementService;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -16,11 +20,30 @@ import java.util.UUID;
 @Slf4j
 public class ReplacementManagementServiceImpl implements ReplacementManagementService {
     private final UserRepository userRepository;
+    private final DepartmentRepository departmentRepository;
 
-    @Override
+    public List<User> getAllUsersForReplacement(UUID userId) {
+        Optional<Long> departmentByManagerUuid = departmentRepository.findDepartmentByManagerUuid(userId);
+        Optional<Long> departmentByMemberUuid = departmentRepository.findDepartmentByMemberUuid(userId);
+        if (!departmentByManagerUuid.isEmpty()) {
+            List<User> departmentMembersForReplacementManager = userRepository.getDepartmentMembersForReplacementManager(departmentByManagerUuid.get());
+            return departmentMembersForReplacementManager;
+        } else {
+            List<User> departmentManagersForReplacementDepartmentMember = userRepository.getDepartmentManagersForReplacementDepartmentMember(departmentByMemberUuid.get());
+            List<User> departmentMembersForReplacement = userRepository.getDepartmentMembersForReplacement(userId, departmentByMemberUuid.get());
+            departmentMembersForReplacement.addAll(departmentManagersForReplacementDepartmentMember);
+            return departmentMembersForReplacement;
+        }
+    }
+
     public Page<User> getAllUsersForReplacement(int page, int size, UUID userId) {
+        List<User> users = getAllUsersForReplacement(userId);
         PageRequest pageable = PageRequest.of(page, size);
-        log.info("Get a page of users available for replacement - page: {}, size: {}", page, size);
-        return userRepository.getAllUsersForReplacement(userId, pageable);
+        int start = (int) pageable.getOffset();
+        int end = Math.min((start + pageable.getPageSize()), users.size());
+
+        users.subList(start, end);
+
+        return new PageImpl<>(users, pageable, users.size());
     }
 }
