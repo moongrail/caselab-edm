4f22480efe3fb42bbad87c8a8c997f156f1bfaee
ndtm1 <degtyarevn5@gmail.com>
2024-11-20 01:23:13 +0400

    fix: import

--- backend/src/main/java/ru/caselab/edm/backend/entity/User.java
+++ backend/src/main/java/ru/caselab/edm/backend/entity/User.java
@@ -9,6 +9,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
