42cfc37879c916e71f1879fafa6ab8f882876450
ndtm1 <degtyarevn5@gmail.com>
2024-11-20 01:22:58 +0400

    fix: import

--- backend/src/main/java/ru/caselab/edm/backend/repository/DepartmentRepository.java
+++ backend/src/main/java/ru/caselab/edm/backend/repository/DepartmentRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.caselab.edm.backend.entity.Department;
