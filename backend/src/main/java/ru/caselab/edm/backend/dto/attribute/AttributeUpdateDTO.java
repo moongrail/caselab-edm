f911e1406acda78e4252713000def07b5dbb4074
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-11-20 13:22:25 +0400

    fix: Add '@AllArgsConstructor' & increase min value to 2 for '@Size'

--- backend/src/main/java/ru/caselab/edm/backend/dto/attribute/AttributeUpdateDTO.java
+++ backend/src/main/java/ru/caselab/edm/backend/dto/attribute/AttributeUpdateDTO.java
@@ -3,22 +3,24 @@ package ru.caselab.edm.backend.dto.attribute;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.Set;
 
 @Data
+@AllArgsConstructor
 @Schema(description = "DTO for update attribute")
 public class AttributeUpdateDTO {
 
     @Schema(description = "Attribute name", example = "attribute")
     @NotBlank
-    @Size(min = 1, max = 255, message = "Attribute name must be between {min} and {max} character length")
+    @Size(min = 2, max = 255, message = "Attribute name must be between {min} and {max} character length")
     private String name;
 
     @Schema(description = "Attribute data type", example = "pdf")
     @NotBlank
-    @Size(min = 1, max = 255, message = "Attribute data type must be between {min} and {max} character length")
+    @Size(min = 2, max = 255, message = "Attribute data type must be between {min} and {max} character length")
     private String dataType;
 
     @Schema(description = "Is it required attribute", example = "true")
