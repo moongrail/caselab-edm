d7e405c7d82a306a2233452d3dda3efbb4d0b062
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-11-21 08:43:54 +0400

    test: Add base controller for tests

---
+++ backend/src/test/java/ru/caselab/edm/backend/controllers/BaseControllerTest.java
@@ -0,0 +1,59 @@
+package ru.caselab.edm.backend.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.testcontainers.shaded.org.apache.commons.lang3.RandomStringUtils;
+import ru.caselab.edm.backend.entity.UserInfoDetails;
+import ru.caselab.edm.backend.repository.RoleRepository;
+import ru.caselab.edm.backend.repository.elastic.AttributeSearchRepository;
+import ru.caselab.edm.backend.security.service.JwtService;
+
+import java.util.UUID;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+
+public abstract class BaseControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+    @MockBean
+    private AttributeSearchRepository attributeSearchRepository;
+    @MockBean
+    private UserDetailsService userDetailsService;
+    @MockBean
+    private RoleRepository roleRepository;
+    @MockBean
+    private JwtService jwtService;
+    protected ObjectMapper objectMapper;
+
+
+    protected ResultActions performRequest(MockHttpServletRequestBuilder requestBuilder, Object requestBody, UserInfoDetails userInfoDetails) throws Exception {
+        return mockMvc.perform(
+                requestBuilder
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(csrf())
+                        .with(user(userInfoDetails))
+                        .content(writeAsJson(requestBody))
+        );
+    }
+
+    protected ResultActions performRequest(MockHttpServletRequestBuilder requestBuilder, Object requestBody) throws Exception {
+        return mockMvc.perform(
+                requestBuilder
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(csrf())
+                        .content(writeAsJson(requestBody))
+        );
+    }
+
+    private String writeAsJson(Object object) throws Exception {
+        return objectMapper.writeValueAsString(object);
+    }
+}
