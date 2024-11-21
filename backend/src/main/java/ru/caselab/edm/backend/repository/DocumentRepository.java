e4905042ccbcd8937f9870b741860f8d7cbcedbf
larentina <118663828+larentiina@users.noreply.github.com>
2024-11-21 10:13:52 +0300

    feat: add archivation for documents

--- backend/src/main/java/ru/caselab/edm/backend/repository/DocumentRepository.java
+++ backend/src/main/java/ru/caselab/edm/backend/repository/DocumentRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.caselab.edm.backend.dto.document.DocumentOutputAllDocumentsDTO;
 import ru.caselab.edm.backend.entity.Document;
@@ -20,7 +21,7 @@ public interface DocumentRepository extends JpaRepository<Document, Long> {
             ON d.id = dv.documents_id
             LEFT JOIN approvment_process_item api
             ON dv.id = api.document_version_id
-            WHERE d.id = :documentId AND (d.user_id = :userId)
+            WHERE d.id = :documentId AND (d.user_id = :userId) and d.is_archived = false
             """,
             nativeQuery = true)
     Optional<Document> getDocumentForUser(Long documentId, UUID userId);
@@ -32,7 +33,7 @@ public interface DocumentRepository extends JpaRepository<Document, Long> {
             ON d.id = dv.documents_id
             LEFT JOIN approvment_process_item api
             ON dv.id = api.document_version_id
-            WHERE d.id = :documentId AND api.user_id = :userId
+            WHERE d.id = :documentId AND api.user_id = :userId and d.is_archived = false
             """,
             nativeQuery = true)
     Optional<Document> getDocumentWhereUserSignatories(Long documentId, UUID userId);
@@ -56,7 +57,7 @@ public interface DocumentRepository extends JpaRepository<Document, Long> {
              		                               where dv1.document.id = d.id
                                                    order by dv1.createdAt DESC
              		                               LIMIT 1)
-             		    AND api.user.id = :userId
+             		    AND api.user.id = :userId and d.isArchived = false
             """, countQuery = """
             SELECT count(d.id)
              			FROM Document d
@@ -69,7 +70,7 @@ public interface DocumentRepository extends JpaRepository<Document, Long> {
              		                               where dv1.document.id = d.id
                                                    order by dv1.createdAt DESC
              		                               LIMIT 1)
-             		    AND api.user.id = :userId
+             		    AND api.user.id = :userId and d.isArchived = false
             """)
     Page<DocumentOutputAllDocumentsDTO> getAllDocumentWithNameAndStatusProjectionWhereUserSignatories(UUID userId, Pageable pageable);
 
@@ -162,7 +163,7 @@ public interface DocumentRepository extends JpaRepository<Document, Long> {
              		                               where dv1.document.id = d.id
                                                    order by dv1.createdAt DESC
              		                               LIMIT 1)
-             		    AND (u.id = :userId)
+             		    AND (u.id = :userId) and d.isArchived = false
             """, countQuery = """
             SELECT count(d.id)
              			FROM Document d join d.documentVersion dv join User u ON (u.id = d.user.id)
@@ -171,7 +172,7 @@ public interface DocumentRepository extends JpaRepository<Document, Long> {
              		                               where dv1.document.id = d.id
                                                    order by dv1.createdAt DESC
              		                               LIMIT 1)
-             		    AND (u.id = :userId)
+             		    AND (u.id = :userId) and d.isArchived = false
             """)
     Page<DocumentOutputAllDocumentsDTO> getAllDocumentWithNameAndStatusProjectionForUser(UUID userId, Pageable pageable);
 
@@ -182,7 +183,7 @@ public interface DocumentRepository extends JpaRepository<Document, Long> {
             ON d.id = dv.documents_id
             LEFT JOIN approvment_process_item api
             ON dv.id = api.document_version_id
-            WHERE api.user_id = :userId OR d.user_id = :userId
+            WHERE api.user_id = :userId OR d.user_id = :userId and d.is_archived = false
             """, countQuery = """
             SELECT DISTINCT ON (d.id) d.id, d.user_id, d.document_type_id, d.created_at
             FROM documents d
@@ -190,10 +191,42 @@ public interface DocumentRepository extends JpaRepository<Document, Long> {
             ON d.id = dv.documents_id
             LEFT JOIN approvment_process_item api
             ON dv.id = api.document_version_id
-            WHERE api.user_id = :userId OR d.user_id = :userId
+            WHERE api.user_id = :userId OR d.user_id = :userId and d.is_archived = false
             """,
             nativeQuery = true)
     Page<Document> getAllDocumentForUser(UUID userId, Pageable pageable);
 
-    Optional<Document> getDocumentById(Long id);
+    @Query("SELECT d FROM Document d WHERE d.id = :id AND d.isArchived = false")
+    Optional<Document> getDocumentById(@Param("id") Long id);
+
+
+    @Query(value = """
+            SELECT new ru.caselab.edm.backend.dto.document.DocumentOutputAllDocumentsDTO(d.id as id,
+                                                                                            u.login as login,
+                                                                                            d.createdAt as createdAt, 
+                                                                                            dv.documentName as documentName, 
+                                                                                            dv.contentUrl as contentUrl, 
+                                                                                            dv.state as state)
+                        FROM Document d left join d.documentVersion dv left join User u ON (u.id = d.user.id)
+             			WHERE dv.id = (select dv1.id
+             		                               from DocumentVersion dv1
+             		                               where dv1.document.id = d.id
+                                                   order by dv1.createdAt DESC
+             		                               LIMIT 1)
+             		    AND (u.id = :userId) and d.isArchived = true
+            """, countQuery = """
+            SELECT count(d.id)
+             			FROM Document d join d.documentVersion dv join User u ON (u.id = d.user.id)
+             			WHERE dv.id = (select dv1.id
+             		                               from DocumentVersion dv1
+             		                               where dv1.document.id = d.id
+                                                   order by dv1.createdAt DESC
+             		                               LIMIT 1)
+             		    AND (u.id = :userId) and d.isArchived = true 
+            """)
+    Page<DocumentOutputAllDocumentsDTO> getArchivedDocumentsForUser(UUID userId, Pageable pageable);
+
+    Optional<Document> getDocumentByUserIdAndId(UUID user_id, Long id);
+
+
 }
--- backend/src/main/java/ru/caselab/edm/backend/repository/DocumentVersionRepository.java
+++ backend/src/main/java/ru/caselab/edm/backend/repository/DocumentVersionRepository.java
@@ -1,6 +1,8 @@
 package ru.caselab.edm.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.caselab.edm.backend.entity.DocumentVersion;
 
@@ -11,6 +13,7 @@ import java.util.Optional;
 public interface DocumentVersionRepository extends JpaRepository<DocumentVersion, Long> {
     Optional<DocumentVersion> findByDocumentName(String name);
 
-    List<DocumentVersion> findByDocumentId(Long documentId);
+    @Query("SELECT dv FROM DocumentVersion dv JOIN dv.document d WHERE d.id = :documentId AND d.isArchived = false")
+    List<DocumentVersion> findByDocumentId(@Param("documentId")Long documentId);
 
 }
