6cb21a5f0218ce8034bab9c1ed008bd11fd48e40
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-11-21 09:23:05 +0400

    refactor: Rename repository test

--- backend/src/test/java/ru/caselab/edm/backend/repository/ReplacementManagerRepositoryTest.java
+++ backend/src/test/java/ru/caselab/edm/backend/repository/ReplacementManagerRepositoryTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import ru.caselab.edm.backend.entity.Department;
 import ru.caselab.edm.backend.entity.ReplacementManager;
@@ -22,7 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
 @DataJpaTest
-public class ReplacementManagerRepositoryIT {
+public class ReplacementManagerRepositoryTest {
 
     @Autowired
     private DepartmentRepository departmentRepository;
