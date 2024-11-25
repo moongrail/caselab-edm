2e8aa2923c40fb285b6b922fdde6d44f98581e69
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-11-25 11:15:21 +0400

    fix: Add error message handling in PasswordMatcherValidator

--- backend/src/main/java/ru/caselab/edm/backend/validation/validators/PasswordMatcherValidator.java
+++ backend/src/main/java/ru/caselab/edm/backend/validation/validators/PasswordMatcherValidator.java
@@ -2,10 +2,13 @@ package ru.caselab.edm.backend.validation.validators;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import lombok.extern.slf4j.Slf4j;
 import ru.caselab.edm.backend.validation.annotations.PasswordMatcher;
 import ru.caselab.edm.backend.validation.interfaces.PasswordValidatable;
 
 public class PasswordMatcherValidator implements ConstraintValidator<PasswordMatcher, PasswordValidatable> {
+
+    private static final String PASSWORD_CONFIRMATION_PROPERTY_NODE = "passwordConfirmation";
 
     @Override
     public void initialize(PasswordMatcher constraintAnnotation) {
@@ -14,11 +17,19 @@ public class PasswordMatcherValidator implements ConstraintValidator<PasswordMat
 
     @Override
     public boolean isValid(PasswordValidatable value, ConstraintValidatorContext context) {
-        return value != null && passwordsValuesAreNotNull(value) && passwordsValuesAreEquals(value);
+        if (passwordsValuesAreNotNull(value) && passwordsValuesAreEquals(value)) {
+            return true;
+        }
+
+        context.buildConstraintViolationWithTemplate(context.getDefaultConstraintMessageTemplate())
+                .addPropertyNode(PASSWORD_CONFIRMATION_PROPERTY_NODE)
+                .addConstraintViolation();
+
+        return false;
     }
 
     private boolean passwordsValuesAreNotNull(PasswordValidatable value) {
-        return value.getPassword() != null && value.getPasswordConfirmation() != null;
+        return value != null && value.getPassword() != null && value.getPasswordConfirmation() != null;
     }
 
     private boolean passwordsValuesAreEquals(PasswordValidatable value) {
