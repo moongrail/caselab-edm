e068b04f37d384b98829a1176443515302454a41
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-11-28 13:05:33 +0400

    test: Simplify builders with lombok

--- backend/src/test/java/ru/caselab/edm/backend/testutils/builder/user/CreateUserDtoBuilder.java
+++ backend/src/test/java/ru/caselab/edm/backend/testutils/builder/user/CreateUserDtoBuilder.java
@@ -1,12 +1,16 @@
 package ru.caselab.edm.backend.testutils.builder.user;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.With;
 import ru.caselab.edm.backend.testutils.builder.BaseBuilder;
 import ru.caselab.edm.backend.dto.user.CreateUserDTO;
 import ru.caselab.edm.backend.enums.RoleName;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor(staticName = "builder")
+@With
 public class CreateUserDtoBuilder implements BaseBuilder<CreateUserDTO> {
 
     private Long departmentId = 1L;
@@ -19,61 +23,6 @@ public class CreateUserDtoBuilder implements BaseBuilder<CreateUserDTO> {
     private String patronymic = "DefaultPatronymic";
     private String position = "DefaultPosition";
     private RoleName[] roles = new RoleName[]{RoleName.USER};
-
-
-    public static CreateUserDtoBuilder builder() {
-        return new CreateUserDtoBuilder();
-    }
-
-    public CreateUserDtoBuilder withDepartmentId(Long departmentId) {
-        this.departmentId = departmentId;
-        return this;
-    }
-
-    public CreateUserDtoBuilder withLogin(String login) {
-        this.login = login;
-        return this;
-    }
-
-    public CreateUserDtoBuilder withEmail(String email) {
-        this.email = email;
-        return this;
-    }
-
-    public CreateUserDtoBuilder withPassword(String password) {
-        this.password = password;
-        return this;
-    }
-
-    public CreateUserDtoBuilder withPasswordConfirmation(String passwordConfirmation) {
-        this.passwordConfirmation = passwordConfirmation;
-        return this;
-    }
-
-    public CreateUserDtoBuilder withFirstName(String firstName) {
-        this.firstName = firstName;
-        return this;
-    }
-
-    public CreateUserDtoBuilder withLastName(String lastName) {
-        this.lastName = lastName;
-        return this;
-    }
-
-    public CreateUserDtoBuilder withPatronymic(String patronymic) {
-        this.patronymic = patronymic;
-        return this;
-    }
-
-    public CreateUserDtoBuilder withPosition(String position) {
-        this.position = position;
-        return this;
-    }
-
-    public CreateUserDtoBuilder withRoles(RoleName[] roles) {
-        this.roles = roles;
-        return this;
-    }
 
     @Override
     public CreateUserDTO build() {
--- backend/src/test/java/ru/caselab/edm/backend/testutils/builder/user/UpdateUserDtoBuilder.java
+++ backend/src/test/java/ru/caselab/edm/backend/testutils/builder/user/UpdateUserDtoBuilder.java
@@ -1,12 +1,16 @@
 package ru.caselab.edm.backend.testutils.builder.user;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.With;
 import ru.caselab.edm.backend.testutils.builder.BaseBuilder;
 import ru.caselab.edm.backend.dto.user.UpdateUserDTO;
 import ru.caselab.edm.backend.enums.RoleName;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor(staticName = "builder")
+@With
 public class UpdateUserDtoBuilder implements BaseBuilder<UpdateUserDTO> {
 
     private String login = "default_user";
@@ -16,45 +20,6 @@ public class UpdateUserDtoBuilder implements BaseBuilder<UpdateUserDTO> {
     private String patronymic = "DefaultPatronymic";
     private String position = "DefaultPosition";
     private RoleName[] roles = new RoleName[]{RoleName.USER};
-
-    public static UpdateUserDtoBuilder builder() {
-        return new UpdateUserDtoBuilder();
-    }
-
-    public UpdateUserDtoBuilder withLogin(String login) {
-        this.login = login;
-        return this;
-    }
-
-    public UpdateUserDtoBuilder withEmail(String email) {
-        this.email = email;
-        return this;
-    }
-
-    public UpdateUserDtoBuilder withFirstName(String firstName) {
-        this.firstName = firstName;
-        return this;
-    }
-
-    public UpdateUserDtoBuilder withLastName(String lastName) {
-        this.lastName = lastName;
-        return this;
-    }
-
-    public UpdateUserDtoBuilder withPatronymic(String patronymic) {
-        this.patronymic = patronymic;
-        return this;
-    }
-
-    public UpdateUserDtoBuilder withPosition(String position) {
-        this.position = position;
-        return this;
-    }
-
-    public UpdateUserDtoBuilder withRoles(RoleName[] roles) {
-        this.roles = roles;
-        return this;
-    }
 
     @Override
     public UpdateUserDTO build() {
