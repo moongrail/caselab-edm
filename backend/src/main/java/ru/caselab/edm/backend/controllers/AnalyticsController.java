4a56d84fa2b782a85e9e93680d50b02d130ee9b4
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-11-28 13:28:39 +0400

    refactor: Replace page & size parameters with constants for top-10 results

--- backend/src/main/java/ru/caselab/edm/backend/controllers/AnalyticsController.java
+++ backend/src/main/java/ru/caselab/edm/backend/controllers/AnalyticsController.java
@@ -1,5 +1,7 @@
 package ru.caselab.edm.backend.controllers;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,19 +19,20 @@ import java.util.List;
 @RequestMapping("/analytics")
 public class AnalyticsController {
 
+    private static final int DEFAULT_PAGE = 0;
+    private static final int DEFAULT_SIZE = 10;
+
     private final AnalyticsService analyticsService;
 
     @GetMapping("/top-users-by-document-creation")
     public List<TopUsersByDocumentCreationProjection> getTopUsersByDocumentCreation(@RequestParam LocalDate startDate,
-                                                                                    @RequestParam LocalDate endDate,
-                                                                                    @RequestParam(defaultValue = "0") int page,
-                                                                                    @RequestParam(defaultValue = "10") int size) {
+                                                                                    @RequestParam LocalDate endDate) {
 
         if (startDate.isAfter(endDate)) {
             throw new IllegalArgumentException("Invalid date's parameters. Start date bust be before end date");
         }
 
-        PageRequest pageable = PageRequest.of(page, size);
+        PageRequest pageable = PageRequest.of(DEFAULT_PAGE, DEFAULT_SIZE);
         return analyticsService.findTopUsersByDocumentCreation(startDate, endDate, pageable);
     }
 
