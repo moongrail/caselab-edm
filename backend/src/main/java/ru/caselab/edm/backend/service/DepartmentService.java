b74e8b0b58cdff4055fb837a8c001a4a3939a14a
Saynurdinov Shukhrat <saynurdinov1998@gmail.com>
2024-12-06 16:49:03 +0300

    fix:get-department

--- backend/src/main/java/ru/caselab/edm/backend/service/DepartmentService.java
+++ backend/src/main/java/ru/caselab/edm/backend/service/DepartmentService.java
@@ -4,11 +4,12 @@ import ru.caselab.edm.backend.dto.department.*;
 import ru.caselab.edm.backend.dto.user.UserDTO;
 import ru.caselab.edm.backend.dto.user.UserPageDTO;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface DepartmentService {
     DepartmentDTO createDepartment(CreateDepartmentDTO createDepartmentDTO);
-    DepartmentDTO getDepartmentWithUser(UUID id);
+    List<DepartmentDTO> getDepartmentsWithUser(UUID id);
     UserDTO getManagerOfDepartment(Long id);
     UserPageDTO getAllMembersOfDepartment(int page, int size, Long id);
     DepartmentPageDTO getAllSubordinateDepartments(int page, int size, UUID id);
